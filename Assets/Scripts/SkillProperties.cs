de619d4eddbee7609cc7994fef42bc6d15d3f0e1
Lee Feng <phoenixkaze@live.com>
2023-05-09 22:03:22 +0800

    技能属性、生命、战斗系统基类粗略打个样

---
+++ Assets/Scripts/SkillProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName ="Skill", menuName ="Gameplay/Skill", order = 0)]
+    internal class SkillProperties : ScriptableObject
+    {
+        [SerializeField]
+        private string skillName;
+        public string Name => skillName;
+        [Multiline]
+        [SerializeField]
+        private string description;
+        public string Description => description;
+        [SerializeField]
+        public uint damage;
+        public uint Damage => damage;
+        [SerializeField]
+        private uint cost;
+        public uint Cost => cost;
+    }
+}
